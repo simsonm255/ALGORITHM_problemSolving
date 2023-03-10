4a6a2db1cc3b05c597c1f2d13b3231a73c3ceb23
= <=>
2023-03-10 09:42:16 +0530

    .

---
+++ code/leetcode/704. Binary Search/binarySearch.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var t;
+
+var search = function(nums, target) {
+    // t=target;
+    var find = x=>x==target;
+    
+    return nums.findIndex(find);
+
+};
+
+ 
